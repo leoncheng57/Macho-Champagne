0a7402180b983bbec1f6f712a4bd093b119f5a6a
TESLA <57leonardo@gmail.com>
2016-03-05 16:19:12 -0500

    arrow keys work!

--- testingKeys/animate.js
+++ testingKeys/animate.js
@@ -1,48 +1,47 @@
+//TUTORIALS
+//Keyboard: https://www.kirupa.com/html5/keyboard_events_in_javascript.htm
+
 console.log("hello");
-
-// function myFunction(event) {
-//     var x = event.which;
-// }
-// http://www.w3schools.com/jsref/event_key_which.asp
-// https://www.google.com/search?q=leyboar+djavascript&oq=leyboar+djavascript&aqs=chrome..69i57j0l5.2326j0j4&client=ubuntu&sourceid=chrome&es_sm=93&ie=UTF-8
-
-
 
 var c = document.getElementById("pad");
 var ctx = c.getContext("2d");
 
-var clear = function(){
+var clearScreen = function(){
     ctx.clearRect(0, 0, c.width, c.height);
 };
 
-var drawRect = function drawRect(x, y){
+var drawRect = function(x, y){
     ctx.clearRect(0, 0, c.width, c.height);
     ctx.beginPath();
     ctx.fillStyle = "#ff6c24";
-    ctx.rect(x, y, 123, 12);
+    ctx.rect(x, y, 20, 100);
     ctx.stroke();
     ctx.fill();
     ctx.closePath();
 };
 
+var x = 100;
+var y = 100;
+
 setInterval(function(){
-    drawRect(100, 100);
-}, 500);
+    clearScreen();
+    drawRect(x, y);
+}, 0.001);
 
 var keyboardActions = function keyboardActions(e){
-    var k = e.keycode;
+    var k = e.keyCode;
     console.log(k);
     if (k==37){ //left 
-	
+	x-=10;
     }
     else if (k==38){ //up
-
+	y-=10;
     }
     else if (k==39){ //right
-
+	x+=10;
     }
     else if (k==40){ //down
-
+	y+=10;
     }
 }
 
